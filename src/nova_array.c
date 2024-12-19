a40b00ffb7d56bba575e01754bf8fd2c5b559a6e
Myth512 <scorpion957.k@gmail.com>
2024-12-20 01:25:42 +0300

    Add sort method for array

--- src/nova_array.c
+++ src/nova_array.c
@@ -30,8 +30,58 @@ Value novaArrayReverse(int argc, Value *argv) {
     return NIL_VAL;
 }
 
-Value novaArraySort(int argc, Value *argv) {
+static void swap(ObjArray* array, int l, int r) {
+    Value tmp = array->vec.values[l];
+    array->vec.values[l] = array->vec.values[r];
+    array->vec.values[r] = tmp;
+}
 
+// Hoare partition
+int partition(ObjArray *array, int l, int r, bool reverse) {
+    bool (*leftFunc)(Value, Value) = valueLessEqual;
+    bool (*rightFunc)(Value, Value) = valueGreaterEqual;
+    if (reverse) {
+        leftFunc = valueGreaterEqual;
+        rightFunc = valueLessEqual;
+    }
+
+    Value pivot = array->vec.values[r];
+    int p = r;
+    while (l < r) {
+        while (l < r && leftFunc(array->vec.values[l], pivot))
+            l++;
+        while (l < r && rightFunc(array->vec.values[r], pivot))
+            r--;
+        swap(array, l, r);
+    }
+    swap(array, l, p);
+    return l;
+}
+
+static void quicksort(ObjArray *array, int left, int right, bool reverse) {
+    if (left >= right)
+        return;
+
+    int p = partition(array, left, right, reverse);
+    quicksort(array, left, p - 1, reverse);
+    quicksort(array, p + 1, right, reverse);
+}
+
+Value novaArraySort(int argc, Value *argv) {
+    bool reverse = false;
+    if (argc == 1) {
+        if (IS_BOOL(argv[1])) {
+            reverse = AS_BOOL(argv[1]);
+        } else {
+            reportRuntimeError("Reverse must be boolean");
+        }
+    } else if (argc > 1) {
+        reportRuntimeError("At most 1 arguments");
+    }
+
+    ObjArray *array = AS_ARRAY(argv[0]);
+    quicksort(array, 0, array->vec.size - 1, reverse);
+    return NIL_VAL;
 }
 
 // bool compareArrays(ObjArray *a, ObjArray *b) {
